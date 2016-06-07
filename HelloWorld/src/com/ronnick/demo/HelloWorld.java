8da1d0462c128c4a827d599c60760994981f1fd5
ronnick <ronnick@163.com>
2016-06-08 00:44:55 +0800

    add hello-world demo.

---
+++ HelloWorld/src/com/ronnick/demo/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.ronnick.demo;
+
+/**
+ * Created by ronnick on 2016/6/8.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello world.");
+        System.out.println("This is my first java program.");
+    }
+}
